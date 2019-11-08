902643a6ec9b48f5a77d36072194a5598d9384b5
Severin Lawson <severinlawson@gmail.com>
2019-11-08 05:46:22 +0000

    start the Google Maps Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,11 +1,16 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -26,6 +31,22 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+    public void createMapIntent(View view){
+        String uribase = "geo:0,0?q=618 E South St Orlando, FL 32801";
+        try {
+            URI uriBase = new URI(uribase);
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+            mapIntent.setPackage("com.google.android.apps.maps");
+
+            if (mapIntent.resolveActivity(getPackageManager())!= null){
+                startActivity(mapIntent);
+            }
+
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
